b2239c2ab123c78a726246e232c6c1b390b92b98
Leonor <leonor.mendonca.rodrigues@gmail.com>
2025-12-07 15:05:13 +0000

    Implement solution for day 1 part 1

---
+++ 2025/day-01/part-1-sol.py
@@ -0,0 +1,22 @@
+with open('input.txt') as f:
+    position = 50 
+    count = 0
+    
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+        
+        direction = line[0]
+        distance = int(line[1:])
+        
+        if direction == 'L':
+            position = (position - distance) % 100
+        elif direction == 'R':
+            position = (position + distance) % 100
+        
+        # Count if position is 0 after this move
+        if position == 0:
+            count += 1
+    
+    print(count)
